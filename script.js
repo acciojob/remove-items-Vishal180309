5f07005f32313eaa109e96d5a8b76cc0d75fd2e7
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-03-09 14:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function removeSelectedOption() {
+    let colorSelect = document.getElementById('colorSelect');
+    let selectedIndex = colorSelect.selectedIndex;
+    
+    if (selectedIndex !== -1) { // Ensure an option is selected before removing
+        colorSelect.remove(selectedIndex);
+    }
+}
